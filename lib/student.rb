106799a8b4aa80a43302316be5045585d162d22c
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-21 13:14:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,12 @@ class Student
  
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(grade)
+      sql = <<-SQL 
+      SELECT * FROM students 
+      WHERE grade = ?
+    SQL
     
+    DB[:conn].execute(sql)
   end
 end
