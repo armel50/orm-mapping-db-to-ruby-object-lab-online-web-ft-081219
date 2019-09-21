13f6d76fa72b90c8ed7f4bad16ca5048174201c8
Cloe Kouadjo <armelfli50@gmail.com>
2019-09-21 13:01:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,5 +86,10 @@ class Student
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
+  end 
+  
+  def self.first_student_in_grade_10 
   end
+  
+  def self.
 end
